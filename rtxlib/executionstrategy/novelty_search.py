8b0bc604c5f88111fa1a7ca67f28ddeaf78fc82e
Erik Fredericks <fredericks@oakland.edu>
2019-01-18 15:11:17 +0000

    Update novelty search to follow NSGA2 evaluation style

--- rtxlib/executionstrategy/novelty_search.py
+++ rtxlib/executionstrategy/novelty_search.py
@@ -15,6 +15,9 @@ from deap import tools, base, creator
 from collections import defaultdict
 from colorama import Fore
 
+debug = False
+verbose = True
+
 def novelty_search(variables, range_tuples, init_individual, mutate, evaluate, wf):
     # TODO - debug
     filename = "NS-tmp.txt"
@@ -28,6 +31,8 @@ def novelty_search(variables, range_tuples, init_individual, mutate, evaluate, w
 
     fitness_weight        = wf.execution_strategy["fitness_weight"]
     novelty_weight        = wf.execution_strategy["novelty_weight"]
+
+    offspring_size = wf.execution_strategy["offspring_size"]
 
 
 
@@ -52,10 +57,10 @@ def novelty_search(variables, range_tuples, init_individual, mutate, evaluate, w
     toolbox.register("individual", init_individual, variables=variables, range_tuples=range_tuples)
     toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-    pop = toolbox.population(n=population_size)
+    population = toolbox.population(n=population_size)
 
     info("Variables: " + str(variables))
-    info("Population: " + str(pop))
+    info("Population: " + str(population))
 
     toolbox.register("mate", tools.cxOnePoint)
     toolbox.register("mutate", mutate, variables=variables, range_tuples=range_tuples)
@@ -66,25 +71,107 @@ def novelty_search(variables, range_tuples, init_individual, mutate, evaluate, w
     del wf.primary_data_provider["instance"]
     del wf.secondary_data_providers[0]["instance"]
 
+    # log the history
+    history = tools.History()
+    # Decorate the variation operators
+    toolbox.decorate("mate", history.decorator)
+    toolbox.decorate("mutate", history.decorator)
+
     toolbox.register("evaluate", evaluate, vars=variables, ranges=range_tuples, wf=wf)
+
+    # update history
+    history.update(population)
+
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    # axis = 0, the numpy.mean will return an array of results
+    stats.register("avg", np.mean, axis=0)
+    stats.register("std", np.std, axis=0)
+    stats.register("min", np.min, axis=0)
+    stats.register("max", np.max, axis=0)
+    stats.register("pop_fitness", return_as_is)
+
+    logbook = tools.Logbook()
+    logbook.header = ['gen', 'nevals'] + (stats.fields if stats else [])
 
     # Evaluate the entire population
     number_individuals_to_evaluate_in_parallel = wf.execution_strategy["population_size"]
-    zipped = zip(pop, range(number_individuals_to_evaluate_in_parallel), [0]*number_individuals_to_evaluate_in_parallel)
+    zipped = zip(population, range(number_individuals_to_evaluate_in_parallel), [0]*number_individuals_to_evaluate_in_parallel)
     if wf.execution_strategy["parallel_execution_of_individuals"]:
         pool = pathos.multiprocessing.ProcessPool(number_individuals_to_evaluate_in_parallel)
         fitnesses = pool.map(toolbox.evaluate, zipped)
     else:
         fitnesses = map(toolbox.evaluate, zipped)
 
-    for ind, fit in zip(pop, fitnesses):
+    for ind, fit in zip(population, fitnesses):
         info("> " + str(ind) + " -- " + str(fit))
         ind.fitness.values = fit
 
     # Calculate initial novelty archive
-    novelty_archive = calculate_novelty(0, pop, novelty_archive_k, novelty_archive, novelty_weight, fitness_weight)
+    novelty_archive = calculate_novelty(0, population, novelty_archive_k, novelty_archive, novelty_weight, fitness_weight)
+    record = stats.compile(population) if stats is not None else {}
+    logbook.record(gen=0, nevals=len(population), **record)
+    if verbose:
+        info(logbook.stream)
 
-    for g in range(1, optimizer_iterations):
+    for gen in range(1, optimizer_iterations):
+        if debug:
+            info("\n" + str(gen) + ". Generation")
+            info("Population    : " + str(population))
+
+        # Vary the population
+        offspring = vary(population, toolbox, offspring_size, crossover_probability, mutation_probability)
+        if debug:
+            info("Offspring     : " + str(offspring))
+
+        # Evaluate the individuals with an invalid fitness
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        number_individuals_to_evaluate = len(invalid_ind)
+        zipped = zip(invalid_ind, range(number_individuals_to_evaluate), [gen]*number_individuals_to_evaluate)
+        if wf.execution_strategy["parallel_execution_of_individuals"]:
+            # TODO in each generation, a new pool is created. What about the pool created in the previous run(s)?
+            pool = pathos.multiprocessing.ProcessPool(number_individuals_to_evaluate)
+            fitnesses = pool.map(toolbox.evaluate, zipped)
+        else:
+            fitnesses = map(toolbox.evaluate, zipped)
+
+        # assign fitness to individuals
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        # Update the hall of fame with the generated individuals
+#        if hall_of_fame is not None:
+#            hall_of_fame.update(offspring)
+
+        # Select the next generation population
+        population[:] = toolbox.select(population + offspring, population_size)
+        if debug:
+            info("New Population: " + str(population))
+
+        if debug:
+            for i in range(len(population)):
+                for j in range(i, len(population)):
+                    if i != j:
+                        dup = is_duplicate(population[i], population[j])
+                        if dup:
+                            info("Duplicate individuals #" + str(i) + " and #" + str(j))
+        novelty_archive = calculate_novelty(gen, population, novelty_archive_k, novelty_archive, novelty_weight, fitness_weight)
+
+        # The population is entirely replaced by the offspring
+        population[:] = offspring
+        info("> Population: " + str(population))
+        info("> Individual: " + str(variables))
+        info("> Novelty archive: " + str(novelty_archive))
+
+        # Update the statistics with the new population
+        record = stats.compile(population) if stats is not None else {}
+        record['novelty_archive'] = novelty_archive
+
+        logbook.record(gen=gen, nevals=len(invalid_ind), **record)
+        if verbose:
+            info(logbook.stream)
+        """
+        # This is the old version where we weren't getting full evaluation.  It can be removed once we verify that everything is OK
+
         info("> \n" + str(g) + ". Generation")
         # Select the next generation individuals
         offspring = toolbox.select(pop, len(pop))
@@ -124,10 +211,11 @@ def novelty_search(variables, range_tuples, init_individual, mutate, evaluate, w
         info("> Population: " + str(pop))
         info("> Individual: " + str(variables))
         info("> Novelty archive: " + str(novelty_archive))
+        """
 
         # TODO : debugging
         with open(filename, 'a') as f:
-          f.write("Generation %d\n" % g)
+          f.write("Generation %d\n" % gen)
           for na in novelty_archive:
             f.write("%s\n" % str(na))
           f.write("\n")
@@ -190,3 +278,43 @@ def calculate_manhattan_distance(ind1, ind2):
   ind1_a = np.array(ind1)
   ind2_a = np.array(ind2)
   return np.linalg.norm(ind1_a - ind2_a)
+
+def vary(population, toolbox, lambda_, cxpb, mutpb):
+    assert (cxpb + mutpb) <= 1.0, ("The sum of the crossover and mutation probabilities must be smaller or equal to "
+                                   "1.0.")
+
+    offspring = []
+    for _ in xrange(lambda_):
+        op_choice = random.random()
+        if op_choice < cxpb:  # Apply crossover
+            ind1, ind2 = map(toolbox.clone, random.sample(population, 2))
+            msg = str(ind1) + " x " + str(ind2)
+            ind1, ind2 = toolbox.mate(ind1, ind2)
+            del ind1.fitness.values
+            offspring.append(ind1)
+            if debug:
+                info("Obtained by crossover " + str(ind1) + " from " + msg)
+        elif op_choice < cxpb + mutpb:  # Apply mutation
+            ind = toolbox.clone(random.choice(population))
+            msg = str(ind)
+            ind, = toolbox.mutate(ind)
+            del ind.fitness.values
+            offspring.append(ind)
+            if debug:
+                info("Obtained by mutation " + str(ind) + " from " + msg)
+        else:  # Apply reproduction
+            offspring.append(random.choice(population))
+
+    return offspring
+
+
+def return_as_is(a):
+    return a
+
+
+def is_duplicate(ind1, ind2):
+    for i in range(len(ind1)):
+        if ind1[i] != ind2[i]:
+            return False
+
+    return True
